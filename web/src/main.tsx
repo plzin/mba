14945e4b72bc05d987e168f9932fa58f67d52872
Justus Polzin <justuspolzin@gmail.com>
2025-07-14 15:37:23 +0200

    Use local katex css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './app.tsx';
+import './index.css';
+import 'katex/dist/katex.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
